977f0768a885499b227c76567a8a560366f321d7
David Grigorian <david.grigorian.2200@gmail.com>
2024-01-08 16:50:39 +0300

    fix sidebar tests

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,15 +1,15 @@
 import { fireEvent, screen } from "@testing-library/react";
 import { Sidebar } from "widgets/Sidebar";
-import { renderWithTranslation } from "shared/lib/tests/renderWithTranslation/renderWithTranslation";
+import { renderComponent } from "shared/lib/tests/renderComponent/renderComponent";
 
 describe("Sidebar", function () {
     test("render sidebar", () => {
-        renderWithTranslation(<Sidebar />);
+        renderComponent(<Sidebar />);
         expect(screen.getByTestId("sidebar")).toBeInTheDocument();
     });
 
     test("toggle collapse button", () => {
-        renderWithTranslation(<Sidebar />);
+        renderComponent(<Sidebar />);
         const toggleBtn = screen.getByTestId("sidebar-toggle");
         expect(screen.getByTestId("sidebar")).toBeInTheDocument();
         fireEvent.click(toggleBtn);
