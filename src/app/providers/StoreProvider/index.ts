5704adeaca761df87e4e6c26b4e61c84a3668a0b
David Grigorian <david.grigorian.2200@gmail.com>
2024-01-09 21:21:38 +0300

    aadd redux toolkit and counter example

---
+++ src/app/providers/StoreProvider/index.ts
@@ -0,0 +1,5 @@
+import { StoreProvider } from "app/providers/StoreProvider/ui/StoreProvider";
+import { createReduxStore } from "app/providers/StoreProvider/config/store";
+import type { StateSchema } from "./config/StateSchema";
+
+export { StoreProvider, createReduxStore, StateSchema };
