51943be136826da0ab39e63240704e963db36741
David Grigorian <david.grigorian.2200@gmail.com>
2024-01-10 18:12:45 +0300

    fix modal themes

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -64,12 +64,11 @@ export const Modal = (props: ModalProps) => {
     const modes: Record<string, boolean> = {
         [cls.opened]: isOpen,
         [cls.isClosing]: isClosing,
-        [cls[theme]]: true,
     };
 
     return (
         <Portal>
-            <div className={classNames(cls.Modal, modes, [className])}>
+            <div className={classNames(cls.Modal, modes, [className, theme])}>
                 <div className={cls.overlay} onClick={closeHandler}>
                     <div onClick={onContentClick} className={cls.content}>
                         {children}
